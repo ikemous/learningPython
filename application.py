4ee2aadf250eefa1998c20f3b541a6a5ad24af70
Ikemous <barranco.ike@outlook.com>
2021-08-25 00:37:01 -0600

    added seetings for the window

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import sys;
 import pygame;
+from settings import Settings;
 
 class Application:
     ''' Over class to manage game assets and behaviour '''
@@ -7,17 +8,29 @@ class Application:
     def __init__(self):
         ''' initialize the gmae, and create game resources '''
         pygame.init();
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN);
-        pygame.display.set_caption("");
+        self.settings = Settings();
+        self.screen = pygame.display.set_mode((self.settings.screenWidth, self.settings.screenHeight));
+        self.icon = pygame.image.load(self.settings.appImage);
+        pygame.display.set_icon(self.icon);
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN);
+        pygame.display.set_caption(self.settings.caption);
+
+    def checkEvents(self):
+        ''' Respond To Keypresses and mouse events '''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit();
+            
+    def updateScreen(self):
+        self.screen.fill(self.settings.backgroundColor);
+        pygame.display.flip();
 
     def runGame(self):
         ''' Star the main loop for the game '''
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit();
-                
-                pygame.display.flip();
+            self.checkEvents();       
+            self.updateScreen();
+            pygame.display.update();
 
 if __name__ == '__main__':
     # Make a game instance and run the game
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    ''' Class To Hold All Settings For The Application '''
+    def __init__(self):
+        ''' Initialize the settings class'''
+        # Window Settings
+        self.caption = "Space ship";
+        self.screenWidth = 1200;
+        self.screenHeight = 800;
+        self.backgroundColor = (100, 50, 10);
+        self.appImage = "images/beetle.png";
+
