cb531b0693782dac3c9d2ffa730245cde5326bcc
Ikemous <barranco.ike@outlook.com>
2021-08-28 15:27:12 -0600

    attempts to making the image follow the mouse

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame;
+from math import sqrt, acos, degrees, hypot, atan2;
 from settings import Settings;
 
 class Player:
@@ -18,6 +19,9 @@ class Player:
         self.rect.midbottom = self.screenRect.midbottom;
         self.x = float(self.rect.x);
         self.y = float(self.rect.y);
+        self.lastMousePos = (self.screenRect.centerx, 0);
+        self.currentMousePos = (0, 0);
+
         self.rotateAngle = 0;
 
         # Movement Flags
@@ -25,10 +29,23 @@ class Player:
         self.movingLeft = False;
         self.movingUp = False;
         self.movingDown = False;
-    
-    def turnPlayer(self):
-        mousePosition = pygame.mouse.get_pos();
-        print(mousePosition, (self.rect.x, self.rect.y))
+
+    def calculateSideLength(self, a, b):
+        ''' Caluclate the length between two points '''
+        # distance between two points sqrt((X2 - X1)^2 + (Y2 - Y1)^2)
+        return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2);
+
+    def calculateRotateAngle(self):
+        ''' Calculate rotation from old and new mouse posisitons '''
+        playerPos = (self.rect.x, self.rect.y);
+        mousePos = pygame.mouse.get_pos();
+        
+        sideA = self.calculateSideLength(self.lastMousePos, playerPos);
+        sideB = self.calculateSideLength(self.lastMousePos, pygame.mouse.get_pos());
+        sideC = self.calculateSideLength(playerPos, pygame.mouse.get_pos());
+        angle = atan2(mousePos[1] - playerPos[1], mousePos[0] - playerPos[0]);
+        print(f"Player Point:{playerPos} Mouse Point:{mousePos} Angle: {self.rotateAngle}");
+        self.rotateAngle = degrees(angle);
 
     def update(self):
         ''' Update the players position based on the movement flag '''
@@ -43,9 +60,8 @@ class Player:
         # Update the players x and y position
         self.rect.x = self.x;
         self.rect.y = self.y;
-        self.turnPlayer();
-        self.image = pygame.transform.rotate(self.image, 90);
+        self.calculateRotateAngle();
 
     def blitme(self):
         ''' Draw the player at the current position '''
-        self.screen.blit(self.image, self.rect);
+        self.screen.blit(pygame.transform.rotate(self.image, self.rotateAngle), self.rect);
